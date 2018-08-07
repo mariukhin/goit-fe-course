0310119a01e8e9ce52da48c9dc056b75bf7a0619
maxim_mariukhin <maryuhin.z@gmail.com>
2018-08-07 20:26:08 +0300

    js module-1  dz

---
+++ javascript/module-1/module-1.js
@@ -0,0 +1,21 @@
+// let name;
+// let guest;
+
+// name = 'Mango';
+// guest = name;
+
+// console.log(guest);
+
+
+// const day = 07;
+// const month = 08;
+// const year = 2018;
+// const date = day + "\\" + month + "\\" + year;
+// console.log(date);
+
+// let getName = prompt("Name: ", '');
+// alert(getName);
+
+
+
+
