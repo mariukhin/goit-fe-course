8bb2e670c87245ec2cc052c69108077dcaba5267
maxim_mariukhin <maryuhin.z@gmail.com>
2018-08-09 12:15:01 +0300

    js module-1  dz

---
+++ javascript/module-1/module-1-dop.js
@@ -0,0 +1,87 @@
+// let name;
+// let guest;
+// name = 'Mango';
+// guest = name;
+// console.log(guest);
+
+// const day = 07;
+// const month = 08;
+// const year = 2018;
+// const date = day + "\\" + month + "\\" + year;
+
+// console.log(date);
+// let getName = prompt("Name: ", '');
+// alert(getName);
+
+// const name = 'Mango';
+// const date = '14/08/2031';
+// const roomType = 'люкс';
+// const message = `${name} прибывает на отдых ${date} в ${roomType}`;
+// console.log(message);
+
+// const padding = Number.parseInt("20px");
+// const border = Number.parseInt("5px");
+// const contentWidth = Number.parseInt("100px");
+// let totalWidth = padding+border+contentWidth + 'px';
+// console.log(totalWidth);
+
+// let getYear = prompt("Какой сейчас год?", '');
+// if(getYear === String(2018)){
+//     alert("Все верно!");
+// }else{
+//     alert("Но ведь на дворе 2018!");
+// }
+
+// let getNumber = prompt("Введите произвольное целое число", '');
+// if(getNumber === null){
+//     alert('Приходите еще!');
+// }else if(!String(getNumber).includes('.'), Number.getInteger(getNumber)){
+//     alert('Спасибо!');
+// }else{
+//     alert('Введите целое число!');
+// }
+
+// const num = Number.parseInt(Math.random()*100);
+// let type = num%2 === 0 ? "even" : "odd";
+// console.log(`${num} is ${type}`);
+
+// let hours = 9;
+// let minutes = 10; 
+// let seconds = 42;
+
+// if(hours>=0 && hours<=9){
+//     hours = '0'+ hours;
+// }else{
+//     hours = hours;
+// }
+// if(minutes>=0 && minutes<=9){
+//     minutes = '0'+ minutes;
+// }else{
+//     minutes = minutes;
+// }
+// if(seconds>=0 && seconds<=9){
+//     seconds = '0'+ seconds;
+// }else{
+//     seconds = seconds;
+// }
+// const time = `${hours}:${minutes}:${seconds}`;
+// console.log('Time is: ', time);
+
+// let getHotel = prompt("Введите число от 1 до 5: ", '');
+// if(getHotel<=5 && getHotel>=1){
+//     switch(getHotel){
+//         case '1':alert('Каталог хостелов');
+//                 break;
+//         case '2': alert('Каталог бюджетных отелей');
+//                 break;
+//         case '3': alert('Каталог отелей ***');
+//                 break;  
+//         case '4': alert('Каталог отелей ****');
+//                 break;
+//         case '5': alert('Каталог лучших отелей');
+//     }
+// }else if(getHotel === null){
+//     alert('Очень жаль, приходите еще!');
+// }else{
+//     alert('Неверный ввод, возможные варианты 1-5!');
+// }
