876962d883c86cb63aa7f54a0580ca249e9abe2a
maxim_mariukhin <maryuhin.z@gmail.com>
2018-08-13 00:00:23 +0300

    js module-2 update

--- javascript/module-2/module-2.js
+++ javascript/module-2/module-2.js
@@ -20,3 +20,21 @@ for (let iterator in numbers) {
     total += numbers[iterator];
 }
 console.log(total);
+
+// const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4ssw0rd'];
+// let attempts = 3;
+// let userInput;
+// do {
+//     userInput = prompt('Введите пароль','');
+//     attempts -= 1;
+//     if(passwords.includes(userInput)){
+//         alert('Добро пожаловать!');
+//         break;
+//     }else if(attempts === 0){
+//         alert('У вас закончились попытки, аккаунт заблокирован!');
+//         break;
+//     }else{
+//         alert(`Неверный пароль, у вас осталось ${attempts} попыток`);
+//         continue;
+//     }
+// } while (userInput !== null && attempts !== 0);
