c2f18b9ce3f76527d1f73527429a910077e0522c
maxim_mariukhin <maryuhin.z@gmail.com>
2018-08-13 22:53:09 +0300

    js module-3 dz

---
+++ javascript/module-3/module-03-dz.js
@@ -0,0 +1,11 @@
+// const checkNumberType = function(num) {
+//     if(num % 2 === 0){
+//         const even = "Even";
+//         return even;
+//     }else{
+//         const odd = "Odd";
+//         return odd;
+//     }
+// }
+// console.log(checkNumberType(5));
+
