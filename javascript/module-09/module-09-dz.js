29175abd338ef863f55b6c149369ecf805485c38
maxim_mariukhin <maryuhin.z@gmail.com>
2018-09-08 18:52:18 +0300

    module-09 dz

--- javascript/module-09/module-09-dz.js
+++ javascript/module-09/module-09-dz.js
@@ -43,73 +43,96 @@
 //     getFormattedTime(1523621244239)
 //   ); // 07:24.2
 
-const clockface = document.querySelector(".js-clockface");
-const startBtn = document.querySelector(".js-timer-start");
-const stopBtn = document.querySelector(".js-timer-stop");
+// const clockface = document.querySelector(".js-clockface");
+// const startBtn = document.querySelector(".js-timer-start");
+// const stopBtn = document.querySelector(".js-timer-stop");
 
-const timer = {
-  startTime: null,
-  deltaTime: null,
-  id: null
-};
+// const timer = {
+//   startTime: null,
+//   deltaTime: null,
+//   pauseTime: 0,
+//   id: null
+// };
 
 
-const startTimer = ({target}) => {
-    setActiveBtn(target);
-    timer.id = setInterval(() => {
-        if(timer.deltaTime === null){
-            let date = new Date();
-            updateClockface(clockface, date.getTime());
-        }else{
-            updateClockface(clockface, timer.deltaTime);
-        }
-    },100);
-}
-startBtn.addEventListener('click', startTimer);
+// const startTimer = ({target}) => {
+//     setActiveBtn(target);
+//     timer.startTime = Date.now();
+//     timer.id = setInterval(() => {
+//         const currentTime = Date.now();
+//         timer.deltaTime = currentTime - (timer.startTime+timer.pauseTime);
+//         updateClockface(clockface, timer.deltaTime);
+//     },100);
+// }
+// startBtn.addEventListener('click', startTimer);
 
-const stopTimer = ({target}) => {
-    setActiveBtn(target);
-    timer.deltaTime = clockface.textContent;
-    clearInterval(timer.id);
-}
-stopBtn.addEventListener('click', stopTimer);
-/*
-* Обновляет поле счетчика новым значением при вызове
-* аргумент time это кол-во миллисекунд
-*/
-function updateClockface(elem, time) {
-  // Используйте функцию getFormattedTime из задания #1
-  if(typeof time === 'number'){
-    elem.textContent = getFormattedTime(time);
-  }else{
-    elem.textContent = time;
-  }
-}
-function getFormattedTime(time) {
-    let date = new Date();
-    date.setTime(time);
-    let minutes = addFirstZero(date.getMinutes());
-    let seconds = addFirstZero(date.getSeconds());
-    let millisec = String(date.getMilliseconds()).split('');
-    return `${minutes}:${seconds}.${millisec[0]}`;
-}
-const addFirstZero = (number) => {
-    if(number < 10 && number >= 0){
-        number = String('0'+ number);
-        return number;
-    }
-    return number;
-}
-/*
-* Подсветка активной кнопки
-*/
-function setActiveBtn(target) {
-  if(target.classList.contains('active')) {
-    return;
-  }
-  
-  startBtn.classList.remove('active');
-  stopBtn.classList.remove('active');
-  
-  target.classList.add('active');
-}
+// const stopTimer = ({target}) => {
+//     setActiveBtn(target);
+//     clearInterval(timer.id);
+//     // timer.pauseTime = Date.now();
+//     updateClockface(clockface, timer.deltaTime);
+// }
+// stopBtn.addEventListener('click', stopTimer);
+
+
+// function updateClockface(elem, time) {
+//   elem.textContent = getFormattedTime(time);
+// }
+// function getFormattedTime(time) {
+//     let date = new Date();
+//     date.setTime(time);
+//     let minutes = addFirstZero(date.getMinutes());
+//     let seconds = addFirstZero(date.getSeconds());
+//     let millisec = Number.parseInt(date.getMilliseconds()/100);
+//     return `${minutes}:${seconds}.${millisec}`;
+// }
+// const addFirstZero = (number) => {
+//     if(number < 10 && number >= 0){
+//         number = String('0'+ number);
+//         return number;
+//     }
+//     return number;
+// }
+
+// function setActiveBtn(target) {
+//   if(target.classList.contains('active')) {
+//     return;
+//   }
+//   startBtn.classList.remove('active');
+//   stopBtn.classList.remove('active');
+//   target.classList.add('active');
+// }
+
+const DELAY = 1000;
+
+let goodsAmount = 100;
+
+const processOrder = (amount) => {
+    return new Promise((onResolve) => {
+        setTimeout(() => {
+            if(typeof amount !== 'number'){
+                throw new Error("Некорректный ввод!");
+            } else if(amount <= goodsAmount){
+                onResolve('Ваш заказ готов!');
+            }else{
+                onResolve('К сожалению на складе недостаточно товаров!');
+            }
+        }, DELAY/2);
+    })
+}; 
+// Вызовы функции для проверки
+processOrder(50)
+  .then(x => console.log(x)) // Ваш заказ готов!
+  .catch(err => console.log(err));
+
+processOrder(50)
+  .then(x => console.log(x)) // Ваш заказ готов!
+  .catch(err => console.log(err));
+
+processOrder(500)
+  .then(x => console.log(x)) // К сожалению на складе недостаточно товаров!
+  .catch(err => console.log(err));
+
+processOrder("qwe")
+  .then(x => console.log(x))
+  .catch(err => console.log(err)); // Некоректный ввод!
